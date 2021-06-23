6177730f3d08c4fceb62cf5a6a54a81afdd26b1e
Emirhan <emirhan.cifci@mobitek.com>
2021-06-23 14:14:38 +0300

    Basic RabbitMQ App

---
+++ Consumer/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://localhost:5672/");
+
+            IConnection conn = factory.CreateConnection();
+            IModel channel = conn.CreateModel();
+            channel.QueueDeclare("queue", false, false, false, null);
+            channel.ExchangeDeclare("direct", ExchangeType.Direct);
+            channel.QueueBind("queue", "direct", "queRoute");
+            Console.WriteLine("Consumer Çalıştı : ");
+            while (true)
+            {
+                CreateConsumer(channel);
+
+            }
+        }
+
+        public static void CreateConsumer(IModel channel)
+        {
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (ch, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                Console.WriteLine(System.Text.Encoding.Default.GetString(body));
+                // copy or deserialise the payload
+                // and process the message
+                // ...
+                channel.BasicAck(ea.DeliveryTag, false);
+            };
+            // this consumer tag identifies the subscription
+            // when it has to be cancelled
+            String consumerTag = channel.BasicConsume("queue", false, consumer);
+        }
+    }
+}
---
+++ RabbitMq/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://localhost:5672/");
+
+            IConnection conn = factory.CreateConnection();
+            IModel channel = conn.CreateModel();
+            channel.QueueDeclare("queue", false, false, false, null);
+            channel.ExchangeDeclare("direct", ExchangeType.Direct);
+            channel.QueueBind("queue", "direct", "queRoute");
+            while (true)
+            {
+                CreatePublisher(channel);
+            }
+
+        }
+
+        public static void CreatePublisher(IModel channel)
+        {
+            byte[] messageBodyBytes = System.Text.Encoding.UTF8.GetBytes("Hello, world!");
+            channel.BasicPublish("direct", "queRoute", null, messageBodyBytes);
+        }
+       
+    }
+}
